d9b9e26347ebc2e5768f8f6d57c6025d9d64badf
counterpost <century_boys@yahoo.co.jp>
2026-03-02 21:16:31 +0900

    0.2.7

--- scripts/babele-register.js
+++ scripts/babele-register.js
@@ -1,7 +1,9 @@
 Hooks.once("babele.init", (babele) => {
+  console.log("[pf2e-ja] babele.init hook fired");
   babele.register({
     module: "pf2e-ja",
     lang: "ja",
-    dir: "babele/ja/compendium"
+    dir: "babele/ja"
   });
+  console.log("[pf2e-ja] babele.register called");
 });
